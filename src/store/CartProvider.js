2b961908bb5a1cfd4666346d94b22910aa1b70e7
anask601 <anask60155@gmail.com>
2022-02-21 22:10:31 +0530

    adding items complex logic useReducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,8 +8,26 @@ const defaultCartState = {
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
     const updateItems = state.items.concat(action.item);
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
     const updateTotalAmount =
       state.totalAmount + action.item.amount * action.item.price;
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
     return {
       items: updateItems,
       totalAmount: updateTotalAmount,
