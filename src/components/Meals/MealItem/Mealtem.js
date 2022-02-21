3ce972e9e93414d79ac2412d2230a72f8e8fbbbd
anask601 <anask60155@gmail.com>
2022-02-21 14:01:56 +0530

    added form input

--- src/components/Meals/MealItem/Mealtem.js
+++ src/components/Meals/MealItem/Mealtem.js
@@ -1,5 +1,7 @@
 import React from "react";
 import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
+
 const MealItem = ({ name, description, prices }) => {
   return (
     <li className={classes.meal}>
@@ -7,6 +9,9 @@ const MealItem = ({ name, description, prices }) => {
         <h3>{name}</h3>
         <div className={classes.description}>{description}</div>
         <div className={classes.price}>{prices}</div>
+      </div>
+      <div>
+        <MealItemForm />
       </div>
     </li>
   );
