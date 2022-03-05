c2cda848049ab506c9c3c3300f542db3373b0058
anask601 <anask60155@gmail.com>
2022-03-05 22:26:48 +0530

    add loading functionality

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,9 +5,11 @@ import MealItem from "./MealItem/Mealtem";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchMeals = async () => {
+      setIsLoading(true);
       const response = await fetch(
         "https://food-order-app-7fe5f-default-rtdb.firebaseio.com/meals.json"
       );
@@ -23,9 +25,19 @@ const AvailableMeals = () => {
         });
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
+
     fetchMeals();
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealIsLoading}>
+        <p>loading....</p>
+      </section>
+    );
+  }
 
   const mealLists = meals.map((meal) => (
     <MealItem
